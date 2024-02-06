456bed565851f29078f627bd05e84d105c58ef13
PhucTD <trucka312@gmail.com>
2024-02-06 09:09:33 +0700

    push code

--- go-course/15-channel/main.go
+++ go-course/15-channel/main.go
@@ -15,15 +15,14 @@ func task(name string, channel chan int) {
 func main() {
 	channel := make(chan int)
 
-	go task("goroutine 1", channel)
-	go task("goroutine 2", channel)
-	go task("goroutine 3", channel)
-	go task("goroutine 4", channel)
-	go task("goroutine 5", channel)
+	go task("go time 1", channel)
+	go task("go time 2", channel)
+	go task("go time 3", channel)
+	go task("go time 4", channel)
+	go task("go time 5", channel)
 
 	for i := 0; i < 100; i++ {
 		channel <- i
 	}
-	// wait all gorotines to finish
 	time.Sleep(time.Second)
 }
